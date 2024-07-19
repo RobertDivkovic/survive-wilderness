c28a7761ddadbdf6b82a10341886e97590afca05
Robert Divkovic <robertdivkovic98@gmail.com>
2024-07-19 14:16:25 +0000

    Setup structure for game functions

---
+++ assets/js/script.js
@@ -0,0 +1,41 @@
+document.getElementById('startGame').addEventListener('click', startGame);
+
+function startGame() {
+
+}
+
+function updateStatus() {
+
+}
+
+function nextTurn() {
+
+}
+
+function displayeObstacleEvent() {
+
+}
+
+function displayEnemyEvent() {
+
+}
+
+function displayeEvent() {
+
+}
+
+function displayWizardEvent() {
+
+}
+
+function generateWizardQuestion() {
+
+}
+
+function getRandomHealthPack() {
+
+}
+
+function endGame() {
+    
+}

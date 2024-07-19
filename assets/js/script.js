a5acf26c0eb85f503aee6803a8a97ed2082f47ad
Robert Divkovic <robertdivkovic98@gmail.com>
2024-07-19 14:41:24 +0000

    Implement start game function and difficulty selection logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,43 @@
 document.getElementById('startGame').addEventListener('click', startGame);
 
 function startGame() {
+    const playerName = document.getElementById('playerName').value;
+    const difficulty = document.getElementById('difficulty').value;
+    if (!playerName) {
+        alert('Please enter your name to begin your journey.');
+        return;
+    }
+    document.querySelector('.game-intro').classList.add('hidden');
+    document.querySelector('#gameArea').classicList.remove('hidden');
 
+    document.getElementById('playerNameDisplay').innerText = `Name: ${playerName}`;
+    let maxTurns;
+    let health = 1000;
+    let healthMultiplier = 1;
+    let damageMultiplier = 1;
+
+    switch (difficulty) {
+        case 'easy':
+            maxTurns = 20;
+            healthMultiplier = 2;
+            damageMultiplier = 0.5;
+            break;
+        case 'moderate':
+            maxTurns = 25;
+            break;
+        case 'hard':
+            maxTurns = 30;
+            healthMultiplier = 0.5;
+            damageMultiplier = 2;
+            break;
+        case 'custom':
+            maxTurns = prompt("Enter the number of turns (1-50):", 25);
+            if (maxTurns < 1 || maxTurns > 50) maxTurns = 25;
+            break;
+    }
+    let currentTurn = 0;
+    updateStatus(health, currentTurn, maxTurns)
+    nextTurn(health, currentTurn, maxTurns, healthMultiplier, damageMultiplier)
 }
 
 function updateStatus() {
@@ -37,5 +73,5 @@ function getRandomHealthPack() {
 }
 
 function endGame() {
-    
+
 }
